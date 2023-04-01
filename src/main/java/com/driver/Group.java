f7733dbd4fd7680b45b06ded63c84946ed939872
kumar026 <velugulakumar786@gmail.com>
2023-04-01 22:56:17 +0530

    commit 1

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,36 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
     private String name;
     private int numberOfParticipants;
 
+
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+
+    public  String getName() {
+
+        return name;
+    }
+
+    public int getNumberOfParticipants() {
+
+        return numberOfParticipants;
+    }
 }
